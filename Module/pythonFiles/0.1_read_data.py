68cca444024584fbfd5ebea043b73bffc59ba887
kristoffer <kristoffer5740@gmail.com>
2025-12-06 13:15:16 +0100

    refactor: Splitting fetch_data file into chunks

---
+++ Module/pythonFiles/0.1_read_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+import datetime
+
+
+def read_data(data):
+    max_date = "2024-01-31"
+    min_date = "2024-01-01"
+
+    if not max_date:
+        max_date = pd.to_datetime(datetime.datetime.now().date()).date()
+    else:
+        max_date = pd.to_datetime(max_date).date()
+
+    min_date = pd.to_datetime(min_date).date()
+
+    # Time limit data
+    data["date_part"] = pd.to_datetime(data["date_part"]).dt.date
+    data = data[(data["date_part"] >= min_date) & (data["date_part"] <= max_date)]
+
+    min_date = data["date_part"].min()
+    max_date = data["date_part"].max()
+    date_limits = {"min_date": str(min_date), "max_date": str(max_date)}
+    with open("./artifacts/date_limits.json", "w") as f:
+        json.dump(date_limits, f) # Save date intervals
+    
+    return data
